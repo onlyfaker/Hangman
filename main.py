99f439c5318f8ee57c6a67d4129181141ca697fb
Gemaljevic Aleksandar <67740316+onlyfaker@users.noreply.github.com>
2025-02-18 22:16:00 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
 import random
 #todo - switching letters for blanks and printing
-#todo -  life score print
 #todo - expand the the of words to dictionary list
-#todo -
+
 d_list = ["mouse", "crocodile","zebra"]
 hangman = [
     """
@@ -12,7 +11,8 @@ hangman = [
  /|\  |
  / \  |
       |
-=========""","""
+=========
+YOU LOSE H A H A H A H A""","""
   +---+
   |   |
   O   |
@@ -55,36 +55,46 @@ hangman = [
       |
       |
 ========="""]
-word_blanks_letters = []
+word_blanks = []
 #random word from the list choice
 chosen_word = random.choice(d_list)
 print(chosen_word)
 
+letters = list(chosen_word)#letters as a list
+
 #blanks printout
 blanks = len(chosen_word)
 br=0
-print("word to guess:")
 while br!=blanks:
     one_blank = '_'
-    print(one_blank,end=" ")
-    word_blanks_letters.append(one_blank)
+    word_blanks.append(one_blank)
     br+=1
-print()
-print(" ".join(word_blanks_letters))#blanks but in normal mode lol
+normal_word_blanks =  " ".join(word_blanks)
+print("Word to guess: ",normal_word_blanks)#blanks but in normal mode lol
+print(list(normal_word_blanks))#TODO -  use this transformation to acess blanks and change them for letters
 
 #todo - not finished gameplay
 life = 6
+letters_guessed = ""
 while(life!=0):
-    guess = input("guess the letter: ")
-    guess.lower()
+    guess = input("Guess a letter: ")
+    lower_case_guess = guess.lower()
+    letters_guessed+=lower_case_guess
 
-    if (guess in chosen_word):
-        print("right")
+    print(lower_case_guess)
+    if (lower_case_guess in chosen_word):
+
+        print(f"******************{life}/6 lifes left**************")
+        print("Guessed letters: ",",".join(letters_guessed))
         print(hangman[life])
+
     else:
         print('wrong')
         life-=1
+        print(f"******************{life}/6 lifes left**************")
+        print("Guessed letters: ",",".join(letters_guessed))
         print(hangman[life])
+
 
 
 
